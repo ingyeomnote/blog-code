ba195b64f95eaab9d2dd56379f706fdbd1788061
ingyeomnote <79016263+ingyeomnote@users.noreply.github.com>
2024-04-25 09:00:13 +0900

    github 사용자 명 변경

--- readme_update.py
+++ readme_update.py
@@ -12,16 +12,16 @@ markdown_text = """
 ###  🐱 github stats  
 
 <div id="main" align="center">
-    <img src="https://github-readme-stats.vercel.app/api?username=peterica&count_private=true&show_icons=true&theme=radical"
+    <img src="https://github-readme-stats.vercel.app/api?username=ingyeomnote&count_private=true&show_icons=true&theme=radical"
         style="height: auto; margin-left: 20px; margin-right: 20px; padding: 10px;"/>
-    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=peterica&layout=compact"   
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=ingyeomnote&layout=compact"   
         style="height: auto; margin-left: 20px; margin-right: 20px; padding: 10px;"/>
 </div>
 
 ###  💁‍♀️ About Me  
 <p align="center">
     <a href="https://inkyeomnote.tistory.com/"><img src="https://img.shields.io/badge/Blog-FF5722?style=flat-square&logo=Blogger&logoColor=white"/></a>
-    <a href="mailto:kng03318@gmail.com"><img src="https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=ilovefran.ofm@gmail.com"/></a>
+    <a href="mailto:kng03318@gmail.com"><img src="https://img.shields.io/badge/Gmail-d14836?style=flat-square&logo=Gmail&logoColor=white&link=kng03318@gmail.com"/></a>
 </p>
 
 <br>
